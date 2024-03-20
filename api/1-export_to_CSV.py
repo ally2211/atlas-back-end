d07c54ef15489874b9066379550ac23b16652ca0
ally2211 <amendieta.va@gmail.com>
2024-03-19 19:49:53 -0500

    exporting to csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Import the Flask class from the flask module
+"""
+import csv
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_ext = "/users/{}".format(sys.argv[1])
+    todo_ext = "/todos"
+
+    user_response = requests.get(base_url + user_ext)
+    user = user_response.json()
+
+    if user:
+        tasks_response = requests.get(base_url + user_ext + todo_ext)
+        tasks = tasks_response.json()
+
+        USER_ID = user['id']
+        USERNAME = user['name']
+        employeetasks = []
+        
+        for task in tasks:
+                TASK_COMPLETED_STATUS = task['completed']
+                TASK_TITLE = task['title']
+                employeetasks.append([str(USER_ID) + "," + USERNAME + "," + str(TASK_COMPLETED_STATUS) + "," + TASK_TITLE])
+        #print(employeetasks)
+        csvfilename = str(USER_ID) + '.csv'
+        with open(csvfilename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(employeetasks)
+    else:
+        print('Employee not found')
