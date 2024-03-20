3a7526d17b3d12e186018bb845b1e1356181a1f7
ally2211 <amendieta.va@gmail.com>
2024-03-20 01:28:41 -0500

    all users json file

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Import the Flask class from the flask module
+"""
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_ext = "/users"
+    todo_ext = "/todos"
+
+    user_response = requests.get(base_url + user_ext)
+    users = user_response.json()
+    tasks_response = requests.get(base_url + todo_ext)
+    tasks = tasks_response.json()
+
+    USER_DICT = {}
+    USERS = []
+    for user in users:
+        TASKS = []
+        USER_ID = user['id']
+        USERNAME = user['username']
+
+        for task in tasks:
+            TASK_COMPLETED_STATUS = task['completed']
+            TASK_TITLE = task['title']
+            TASKS.append({
+                'username': USERNAME,
+                'task': TASK_TITLE,
+                'completed': TASK_COMPLETED_STATUS
+            })
+
+        # use USER_ID as key, mapping to the TASKS_LIST
+        USER_DICT = {
+            USER_ID: TASKS
+        }
+        USERS.append(USER_DICT)
+    with open("todo_all_employees.json", "w") as outfile:
+        json.dump(USERS, outfile)
