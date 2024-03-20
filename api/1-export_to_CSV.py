2b6f39888679afe4a119acb49a1d52b16d2b87b4
ally2211 <amendieta.va@gmail.com>
2024-03-19 22:30:55 -0500

    change name to username

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     tasks = tasks_response.json()
 
     USER_ID = user['id']
-    USERNAME = user['name']
+    USERNAME = user['username']
     employeetasks = []
 
     if user:
