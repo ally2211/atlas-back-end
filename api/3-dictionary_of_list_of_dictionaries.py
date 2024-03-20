65ab33bf0a1cedb1009d1fd8390bb94cc5f96785
ally2211 <amendieta.va@gmail.com>
2024-03-20 02:51:36 -0500

    add condition that task user same as user id

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -23,14 +23,16 @@ if __name__ == '__main__':
         USER_ID = user['id']
         USERNAME = user['username']
         for task in tasks:
-            TASK_COMPLETED_STATUS = task['completed']
-            TASK_TITLE = task['title']
-            TASKS.append({
-                'username': USERNAME,
-                'task': TASK_TITLE,
-                'completed': TASK_COMPLETED_STATUS
-            })
-        # After populating TASKS for the user, assign it to USER_DICT
-        USER_DICT[USER_ID] = TASKS
+            TASK_USERID = task['userId']
+            if TASK_USERID == USER_ID:
+                TASK_COMPLETED_STATUS = task['completed']
+                TASK_TITLE = task['title']
+                TASKS.append({
+                    'username': USERNAME,
+                    'task': TASK_TITLE,
+                    'completed': TASK_COMPLETED_STATUS
+                })
+            # After populating TASKS for the user, assign it to USER_DICT
+            USER_DICT[USER_ID] = TASKS
     with open("todo_all_employees.json", "w") as outfile:
         json.dump(USER_DICT, outfile)
